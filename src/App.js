8435b959db021e19b9529e9988cdc542054fccc8
Gayanuka Bulegoda <grbulegoda@gmail.com>
2024-07-12 15:17:24 +0530

    src folder update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Router>
+            <Routes>
+                <Route></Route>
+            </Routes>
+        </Router>
+      </div>
   );
 }
 
