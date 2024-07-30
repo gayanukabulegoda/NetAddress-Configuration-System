e8ee6d929e961fc9fcca86bc5bb6b3b3d8646913
jlokitha <lokithajanindu040130@gmail.com>
2024-07-30 17:48:09 +0530

    department table connectceted | data calculation updated

--- src/pages/front-end/HomePage.tsx
+++ src/pages/front-end/HomePage.tsx
@@ -1,8 +1,15 @@
 import '../../css/HomePage.css'
 import Card from "./Card";
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import {Department} from "../back-end/object/Department.tsx";
+import {calculate} from "../back-end/Calculate.tsx";
 
-function HomePage(){
+function HomePage() {
+
+    const [departments, setDepartments] = useState<Department[]>([]);
+    const [name, setName] = useState<string>('');
+    const [count, setCount] = useState<number | string>('');
 
     const navigate = useNavigate();
 
@@ -10,7 +17,21 @@ function HomePage(){
         navigate("/ViewAllPage")
     }
 
-    return(
+    const handleAdd = () => {
+        if (name.trim() !== '' && count !== '' && !isNaN(Number(count))) {
+            const department = new Department(name, Number(count));
+            setDepartments([...departments, department]);
+            setName('');
+            setCount('');
+        }
+    };
+
+    const handleProcess = () => {
+        const departmentData = calculate(departments);
+        console.log(departmentData)
+    }
+
+    return (
         <div id={"HomePage"}>
             <div className="backLayouts">
                 <div></div>
@@ -27,13 +48,13 @@ function HomePage(){
                             <div>
                                 <div>
                                     <h2>Department Name</h2>
-                                    <input type="text"/>
+                                    <input type="text" value={name || ''} onChange={(e) => setName(e.target.value)}/>
                                 </div>
                                 <div>
                                     <h2>User Count</h2>
-                                    <input type="text"/>
+                                    <input type="number" value={count || ''} onChange={(e) => setCount(e.target.value)}/>
                                 </div>
-                                <button>Add</button>
+                                <button onClick={handleAdd}>Add</button>
                             </div>
                         </div>
                         <div className="table">
@@ -46,122 +67,37 @@ function HomePage(){
                                 </thead>
                                 <div>
                                     <tbody>
-                                    <tr>
-                                        <td>
-                                            Develop
-                                        </td>
-                                        <td>
-                                            10
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Develop
-                                        </td>
-                                        <td>
-                                            10
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Develop
-                                        </td>
-                                        <td>
-                                            10
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Develop
-                                        </td>
-                                        <td>
-                                            10
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Develop
-                                        </td>
-                                        <td>
-                                            10
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Develop
-                                        </td>
-                                        <td>
-                                            10
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Develop
-                                        </td>
-                                        <td>
-                                            10
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Develop
-                                        </td>
-                                        <td>
-                                            10
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Develop
-                                        </td>
-                                        <td>
-                                            10
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Develop
-                                        </td>
-                                        <td>
-                                            10
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Develop
-                                        </td>
-                                        <td>
-                                            10
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Develop
-                                        </td>
-                                        <td>
-                                            10
-                                        </td>
-                                    </tr>
+                                    {departments &&
+                                    departments.map(department => (
+                                        <tr>
+                                            <td>
+                                                {department.name}
+                                            </td>
+                                            <td>
+                                                {department.count}
+                                            </td>
+                                        </tr>
+                                    ))}
                                     </tbody>
                                 </div>
                             </table>
                         </div>
-                        <button>Process</button>
+                        <button onClick={handleProcess}>Process</button>
                     </div>
                     <div>
                         <div className="cardset">
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
+                        <Card/>
+                            <Card/>
+                            <Card/>
+                            <Card/>
+                            <Card/>
+                            <Card/>
+                            <Card/>
+                            <Card/>
+                            <Card/>
                         </div>
                         <div className={"viewAll"}>
-                            <button onClick={ handleNavigate }>View All</button>
+                            <button onClick={handleNavigate}>View All</button>
                         </div>
                     </div>
                 </div>
